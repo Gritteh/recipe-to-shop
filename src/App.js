372337b4cd84cd302a7b07d72c1b3bf6375b98e5
jwood <jack.wood@futurenet.com>
2019-12-08 10:36:27 +0000

    main menu styles, basic state functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import MenuItem from './MenuItem/MenuItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			showMenu: true,
+			displayedItem: ''
+		}
+		this.displayItemHideMenu = this.displayItemHideMenu.bind(this)
+	}
+
+	displayItemHideMenu(itemToDisplay) {
+		this.setState({ showMenu: false, displayedItem: itemToDisplay })
+	}
+
+	render() {
+
+		return (
+			<div>
+				{ this.state.showMenu &&
+					<div className="main-menu">
+						<h1 className="main-menu__title">Recipe To Shop</h1>
+						<div className="main-menu__items">
+							<MenuItem
+								itemName="create-recipe"
+								displayText="Create Recipe"
+								showMe={ this.displayItemHideMenu }
+							/>
+							<MenuItem
+								itemName="edit-recipes"
+								displayText="View/ Edit Recipes"
+								showMe={ this.displayItemHideMenu }
+							/>
+							<MenuItem
+								itemName="create-shop"
+								displayText="Create Shopping List"
+								showMe={ this.displayItemHideMenu }
+							/>
+						</div>
+					</div>
+				}
+				{ this.state.displayedItem === 'create-recipe' && 
+					<div>
+						YES
+					</div>
+				}
+				{ this.state.displayedItem === 'edit-recipes' &&
+					<div></div>
+				}
+				{ this.state.displayedItem === 'create-shop' &&
+					<div></div>
+				}
+			</div>
+
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/MenuItem/MenuItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MenuItem.scss'
+
+class MenuItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        const { itemName, displayText, showMe } = this.props
+        return (
+            <div className="menu-item" onClick={ () => showMe(itemName) }>
+                { displayText }
+            </div>
+        )
+    }
+}
+
+export default MenuItem
